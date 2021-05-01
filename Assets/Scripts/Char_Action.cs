e1682fce4f8e4f9a20d4ce4ef965c866da68e2e3
ChanchanCode <pyh254333@gmail.com>
2021-05-01 16:17:16 +0900

    이제 소켓에만 플러그를 꽂을 수 있음, 위쪽으로도 발사 가능, 박스 추가, 꽂기 실패할 때 플러그 튕겨져 나옴, 플러그 밟아서 제거 가능, 조작감 개선(Input 관련한 건 FixedUpdate에서 전부 Update로 옮김)

--- Assets/Scripts/Char_Action.cs
+++ Assets/Scripts/Char_Action.cs
@@ -17,15 +17,18 @@ public class Char_Action : MonoBehaviour
     public float gravityscale;
     bool jumpcanceled = false;
     public LayerMask groundlayer;
+    public LayerMask pluglayer;
     public bool isgrounded = true;
     public Transform groundCheckPoint1;
     public Transform groundCheckPoint2;
     public Transform groundCheckPoint3;
     public Transform CharPlugLinePos;
     public Transform PlugLinePos;
+    public Transform PlugColCheckPos;
     public GameObject Plug;
     public int shootdir = 1;
     public int state;
+    public bool isboxpicked;
 
 
     Rigidbody2D rb;
@@ -44,11 +47,32 @@ public class Char_Action : MonoBehaviour
     // Update is called once per frame조작법
     void Update()
     {
-        xInput = Input.GetAxisRaw("Horizontal");
-    }
-    void FixedUpdate()
-    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (state == 0)
+            {
+                jumpbuffercount = jumpbuffertime;
+            }
+            else if (state == 2 || state == 3)
+            {
+                PlugFail();
+                rb.gravityScale = gravityscale;
+                state = 0;
+                jumpcanceled = false;
+                Jump();
+            }
+        }
+        else
+        {
+            jumpbuffercount -= Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.X) && shoottime > 0.07f)
+        {
+            Shoot();
+        }
         isgrounded = Physics2D.OverlapCircle(groundCheckPoint1.position, 0.015f, groundlayer) || Physics2D.OverlapCircle(groundCheckPoint2.position, 0.015f, groundlayer) || Physics2D.OverlapCircle(groundCheckPoint3.position, 0.015f, groundlayer);
+
+
         if (isgrounded)
         {
             hangcounter = hangtime;
@@ -59,22 +83,11 @@ public class Char_Action : MonoBehaviour
         {
             hangcounter -= Time.deltaTime;
         }
-        if(Input.GetKeyDown(KeyCode.C) && state == 0)
-        {
-            jumpbuffercount = jumpbuffertime;
-        }
-        else
-        {
-            jumpbuffercount -= Time.deltaTime;
-        }
-        
+
+
         if (jumpbuffercount > 0f && hangcounter > 0f)
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpspeed);
-            hangcounter = 0;
-            jumpbuffercount = 0;
-            anim.SetBool("isjumping", true);
-
+            Jump();
         }
         if (!Input.GetKey(KeyCode.C) && rb.velocity.y > 0 && !jumpcanceled)
         {
@@ -91,48 +104,172 @@ public class Char_Action : MonoBehaviour
             sr.flipX = true;
             shootdir = -1;
         }
-        if (Input.GetKeyDown(KeyCode.X) && shoottime > 0.2F)
+
+
+        if (state == 1)
+        {
+            if (Plug.GetComponent<Rigidbody2D>().velocity.magnitude < shootspeed-2f && shoottime > 0.07f)
+            {
+                Collider2D[] col = Physics2D.OverlapCircleAll(PlugColCheckPos.position, 0.15f, groundlayer);
+                bool pluged = false;
+                foreach (Collider2D c in col)
+                {
+                    if (c.gameObject.CompareTag("Socket"))
+                    {
+                        if (Plug.transform.rotation == Quaternion.Euler(0f, 0f, 90f))
+                        {
+                            state = 3;
+                        }
+                        else
+                        {
+                            state = 2;
+                        }
+                        pluged = true;
+                        break;
+                    }
+                    else if (c.gameObject.CompareTag("Box"))
+                    {
+                        PlugFail();
+                        state = 0;
+                        break;
+                    }
+                }
+                if (!pluged)
+                {
+                    PlugFail();
+                    rb.gravityScale = gravityscale;
+                    state = 0;
+                }
+                
+            }
+            else if (shoottime > 1f)
+            {
+                PlugFail();
+                rb.gravityScale = gravityscale;
+                state = 0;
+            }
+
+        }
+
+        shoottime += Time.deltaTime;
+
+        if (state == 1 || state == 2 || state == 3)
+        {
+            lr.enabled = true;
+            if (Plug.transform.rotation == Quaternion.Euler(0f, 0f, 90f))
+            {
+                lr.SetPosition(0, new Vector3(PlugLinePos.position.x, CharPlugLinePos.position.y, 0f));
+                lr.SetPosition(1, new Vector3(PlugLinePos.position.x, PlugLinePos.position.y, 0f));
+            }
+            else
+            {
+                lr.SetPosition(0, new Vector3(CharPlugLinePos.position.x, PlugLinePos.position.y, 0f));
+                lr.SetPosition(1, new Vector3(PlugLinePos.position.x, PlugLinePos.position.y, 0f));
+            }
+
+        }
+        else
+        {
+            lr.enabled = false;
+        }
+        if (state == 3)
+        {
+            rb.velocity = new Vector2(0f, Input.GetAxisRaw("Vertical") * charspeed);
+        }
+        else
+        {
+            rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * charspeed * (state == 1 ? 0 : 1), rb.velocity.y);
+        }
+
+        if (!Plug.GetComponent<Rigidbody2D>().freezeRotation && Vector3.Distance(CharPlugLinePos.position, Plug.transform.position) < 0.5f)
+        {
+            if(Physics2D.OverlapCircle(groundCheckPoint1.position, 0.02f, pluglayer) || Physics2D.OverlapCircle(groundCheckPoint2.position, 0.02f, pluglayer) || Physics2D.OverlapCircle(groundCheckPoint3.position, 0.02f, pluglayer))
+            {
+                Plug.SetActive(false);
+            }
+        }
+
+        anim.SetFloat("xspeed", Mathf.Abs(rb.velocity.x));
+        anim.SetFloat("yspeed", rb.velocity.y);
+    }
+    void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpspeed);
+        hangcounter = 0;
+        jumpbuffercount = 0;
+        anim.SetBool("isjumping", true);
+    }
+    void Shoot()
+    {
+        if (Input.GetAxisRaw("Vertical") > 0)
+        {
+            shoottime = 0;
+            if (state == 0 && isgrounded)
+            {
+                if (!Physics2D.OverlapCircle(new Vector2(CharPlugLinePos.position.x, CharPlugLinePos.position.y + 0.6f), 0.1f, groundlayer))
+                {
+                    PlugReset();
+                    state = 1;
+                    rb.gravityScale = 0;
+                    Plug.transform.position = new Vector2(CharPlugLinePos.position.x, CharPlugLinePos.position.y + 0.6f);
+                    Plug.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, shootspeed);
+                    Plug.transform.localScale = new Vector3(1, 1, 1);
+                    Plug.transform.rotation = Quaternion.Euler(0f, 0f, 90f);
+                }
+            }
+            else if (state == 2 || state == 3)
+            {
+                PlugFail();
+                rb.gravityScale = gravityscale;
+                state = 0;
+            }
+        }
+        else
         {
             shoottime = 0;
             if (state == 0 && isgrounded)
             {
                 if (!Physics2D.OverlapCircle(new Vector2(CharPlugLinePos.position.x + 0.123f * shootdir, CharPlugLinePos.position.y), 0.1f, groundlayer))
                 {
-                    Plug.SetActive(true);
+                    PlugReset();
                     state = 1;
                     rb.gravityScale = 0;
                     Plug.transform.position = new Vector2(CharPlugLinePos.position.x + 0.123f * shootdir, CharPlugLinePos.position.y);
                     Plug.GetComponent<Rigidbody2D>().velocity = new Vector2(shootspeed * shootdir, 0f);
                     Plug.transform.localScale = new Vector3(shootdir, 1, 1);
+                    Plug.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
                 }
             }
-            else if (state == 1)
+            else if (state == 2 || state == 3)
             {
-                Plug.SetActive(false);
+                PlugFail();
                 rb.gravityScale = gravityscale;
                 state = 0;
             }
-
         }
-        shoottime += Time.deltaTime;
-
-
-        if (state == 1 || state == 2)
+    }
+    void PlugFail()
+    {
+        Plug.GetComponent<Rigidbody2D>().gravityScale = gravityscale;
+        Plug.GetComponent<Rigidbody2D>().freezeRotation = false;
+        Plug.GetComponent<Rigidbody2D>().mass = 0.5f;
+        Plug.GetComponent<Rigidbody2D>().AddTorque(Random.Range(-1f, 1f));
+        if (Plug.transform.rotation == Quaternion.Euler(0f, 0f, 90f))
         {
-            lr.enabled = true;
-            lr.SetPosition(0, new Vector3(CharPlugLinePos.position.x, PlugLinePos.position.y, 0f));
-            lr.SetPosition(1, new Vector3(PlugLinePos.position.x, PlugLinePos.position.y, 0f));
+            Plug.GetComponent<Rigidbody2D>().AddForce(new Vector2(Random.Range(-0.2f, 0.2f), -0.2f), ForceMode2D.Impulse);
         }
         else
         {
-            lr.enabled = false;
+            Plug.GetComponent<Rigidbody2D>().AddForce(new Vector2(-shootdir, Random.Range(0.1f, 2f)), ForceMode2D.Impulse);
         }
-
-
-        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * charspeed, rb.velocity.y);
-        anim.SetFloat("xspeed", Mathf.Abs(rb.velocity.x));
-        anim.SetFloat("yspeed", rb.velocity.y);
-
-
+        
+    }
+    void PlugReset()
+    {
+        Plug.SetActive(true);
+        Plug.GetComponent<SpriteRenderer>().sortingOrder = 1;
+        Plug.GetComponent<Rigidbody2D>().mass = 3f;
+        Plug.GetComponent<Rigidbody2D>().gravityScale = 0;
+        Plug.GetComponent<Rigidbody2D>().freezeRotation = true;
     }
 }
